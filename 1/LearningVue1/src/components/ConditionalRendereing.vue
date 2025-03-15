356fd419b02a28e7b3c4742778e0522b8708481f
Abhishek kaul <Abhishekkaul32@gmail.com>
2025-03-15 07:55:06 +0530

    Learnt Conditional rendering

---
+++ 1/LearningVue1/src/components/ConditionalRendereing.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue';
+
+let isVisible = ref(true);
+
+function toggleVisibility() {
+    isVisible.value = !isVisible.value;
+}
+</script>
+
+<template>
+    <p v-show="isVisible">This paragraph will only show or hide when toggled.</p>
+    <button @click="toggleVisibility">Toggle Visibility</button>
+</template>
